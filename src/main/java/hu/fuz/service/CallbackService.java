2e1b3979f3f6617f685770f4e35281d9a6963779
Zsolt Füzesi <zsolt.fuzesi@bankfront.hu>
2018-04-20 11:37:27 +0200

    main class has been added with start function of embedded jetty server fat jar created at phase package swagger json api generated at phase compile

--- src/main/java/hu/fuz/service/CallbackService.java
+++ src/main/java/hu/fuz/service/CallbackService.java
@@ -1,12 +1,16 @@
 package hu.fuz.service;
 
 // a swagger api generálás miatt szükséges
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello_im")
@@ -15,7 +19,10 @@ public class CallbackService {
 
 
     @POST
-    @Consumes(MediaType.TEXT_PLAIN)
+    @Consumes({
+            MediaType.TEXT_PLAIN,
+            MediaType.APPLICATION_JSON})
+    @Produces(MediaType.TEXT_PLAIN)
     @ApiOperation(
             value = "Callback service",
             notes = "This is a simple callback service")
@@ -26,7 +33,9 @@ public class CallbackService {
     @Path("callback")
     public String hello(
             @ApiParam(value = "Body callback message", required = true) String message){
-        System.out.println("I called with message:" + message);
+        System.out.println("<################## I called with message: ##################"
+                + "\n" + message + "\n"
+                + ">################## I called with message: ##################");
         return message;
     }
 }
